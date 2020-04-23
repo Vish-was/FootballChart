e6702dd5bcf8d515ff05e5d12f6c351161535a5d
Vish-was <vshwsnahar3@gmail.com>
2020-04-23 15:16:46 +0530

    Updated index and raw page design

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -3,13 +3,29 @@ class TeamsController < ApplicationController
   # GET /teams.json
   def index
     @teams = Team.all
+    @min_diff = get_min_difference
   end
 
   def raw_data
     @teams = []
-    date_file = TeamsService.new('public/football.dat')
-    date_file.parse do |row|
-      @teams << row
+    # date_file = TeamsService.new('public/football.dat')
+    # date_file.parse do |row|
+    #   @teams << row
+    # end
+    teams = Team.all
+    teams.each do |team|
+      @teams << team.as_json
     end
+    @min_diff = get_min_difference
+  end
+
+  private
+
+  def get_min_difference
+    @teams.sort_by{|team| get_difference(team["goal_hit"], team["goal_against"]).abs}.first
+  end
+
+  def get_difference(goal_hit, goal_against)
+    (goal_hit - goal_against).abs
   end
 end
